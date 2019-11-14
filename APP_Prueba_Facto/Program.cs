71c2629371e8a777ac3aa6e1344360fe525b1981
robinchevez <robinchevez171864gmail.com>
2019-11-14 10:17:00 -0500

    probando factorial en APP Consola

---
+++ APP_Prueba_Facto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_Prueba_Facto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 6;
+            double res = APP_ProgramacionCapas.Factorial.fact(num);
+            Console.WriteLine("El resulttado de : {0} es {1} ",num,res);
+            Console.ReadKey();//pausa
+        }
+    }
+}
