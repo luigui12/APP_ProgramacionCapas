6610c23b0f931637e6d6c86f3c96d62d75cba3de
robinchevez <robinchevez171864gmail.com>
2019-11-14 09:40:09 -0500

    Agregar archivos de proyecto.

---
+++ APP_ProgramacionCapas/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_ProgramacionCapas
+{
+    public static class Factorial
+    {
+        public static long fact(int num)
+        {
+            long f = 1;
+            if (num == 0 || num == 1)
+                return 1;
+            for (int i=1;i<=num;i++) 
+            {
+                f = f * i;
+            }
+            return f;
+        }
+    }
+}
