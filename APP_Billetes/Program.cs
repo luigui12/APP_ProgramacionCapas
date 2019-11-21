fc99a4a2a87b49b4ef35962ce1f6f1a9083eb6a6
luigui2001 <luguijoa09@gmail.com>
2019-11-21 09:23:59 +0100

    App de billetes se ha realizado la llamada de la capa con la respectiva funcion

---
+++ APP_Billetes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_Billetes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cambio(27.50);
+            Console.ReadKey();//pausa
+        }
+
+        static void cambio(double dolares)
+        {
+            int billeta10 = (int)dolares / 10;
+            Console.WriteLine("(0) billetes de 10 dolares", billeta10);
+
+            int billete5 = ((int)dolares % 10);
+            Console.WriteLine("(0) Billete(s) de 5 dolares", billete5);
+
+            int billete1 = ((int)dolares % 10) % 5;
+            Console.WriteLine("(0) billete(s) de 1 dolar", billete1);
+        }
+    }
+}
--- APP_Prueba_Facto/Program.cs
+++ APP_Prueba_Facto/Program.cs
@@ -15,23 +15,6 @@ namespace APP_Prueba_Facto
             Console.WriteLine("El resulttado de : {0} es {1} ",num,res);
             Console.ReadKey();//pausa
 
-            cambio(27.50);
-            Console.ReadKey();
-        }
-        static void cambio(double dolares)
-        {
-            int billeta10 = (int)dolares / 10;
-            Console.WriteLine("(0) billetes de 10 dolares", billeta10);
-
-            int billete5 = ((int)dolares % 10);
-            Console.WriteLine("(0) Billete(s) de 5 dolares", billete5);
-
-            int billete1 = ((int)dolares % 10) % 5;
-            Console.WriteLine("(0) billete(s) de 1 dolar", billete1);
-
-            /*int centavos50 = ();
-            Console.WriteLine("");*/
-
         }
     }
 }
